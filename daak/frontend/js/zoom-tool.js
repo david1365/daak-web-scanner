255232fbe00dc5fb87d6021e78e06792083b66a1
davood <daak1365@yahoo.com>
2018-03-05 18:16:42 +0330

    add placementDaak

--- daak/frontend/js/zoom-tool.js
+++ daak/frontend/js/zoom-tool.js
@@ -4,18 +4,53 @@
 
         body: function () {
             this.mouseDown = false;
-            this.r = 0;
-            // this.owner.addEventListener('click', function () {
-            //     alert(this.zoomTool.innerHTML)
-            // })
 
-            this.knobe.addEventListener('mousedown', function (e) {
+            this.addEventListener('mousemove', function (e) {
                 e.stopPropagation();
-alert(this.mouseDown)
-                this.mouseDown = true;
-                this.r += 5;alert(this.r);
-                this.owner.rotate(this.r);
             })
+
+            this.zKnobe.addEventListener('mousedown', function (e) {
+                this.owner.mouseDown = true;
+            })
+
+            this.zDegree.addEventListener('mousemove', function (e) {
+
+                var
+                    // zKnobe = this.owner.zKnobe,
+                    // zKnobeW = zKnobe.offsetWidth,
+                    // zKnobeH = zKnobe.offsetHeight,
+                    w = this.offsetWidth,
+                    h = this.offsetHeight,
+                    cw = w / 2,
+                    ch = h / 2,
+                    r = cw - 20,
+                    x = e.offsetX,
+                    y = e.offsetY,
+                    realX = x - cw,
+                    realY = y - ch,
+                    // realR = Math.sqrt(Math.pow(-realX, 2) + (Math.pow(-realY, 2))),
+                    RT = realX > 0 && realY > 0 ? true : false,
+                    LT = realX < 0 && realY > 0 ? true : false,
+                    LB = realX < 0 && realY < 0 ? true : false,
+                    RB = realX > 0 && realY < 0 ? true : false,
+                    degree = Math.ceil(Math.degrees(Math.acos(realX / r))),
+                    radian = Math.radians(degree),
+                    newX = r * Math.cos(radian) + r,
+                    newY = r * Math.sin(radian) + r ;
+
+                daak('#inpt').value = x + '<<<+ deg =' + degree + ',' + realX +',' + r/*+ RT + ',' + LT + ',' + LB + ',' + RB*/;
+
+
+                if (this.owner.mouseDown) {
+                    this.owner.zKnobe.left(newX + 3);
+                    this.owner.zKnobe.top(newY);
+                }
+            })
+
+            this.zKnobe.addEventListener('mouseup', function (e) {
+                this.owner.mouseDown = false;
+            })
+
         }
     }
 }) (daak, window, document);
